e81894d547bef4a70f04083ec19fccf5d74a6d9c
cwk <chaewk15@gmail.com>
2023-11-09 18:20:42 +0900

    초기 커밋.

---
+++ FaceDetectionWithOpenCvSharp/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Win32;
+using OpenCvSharp;
+using OpenCvSharp.WpfExtensions;
+using System.Windows;
+
+namespace FaceDetectionWithOpenCvSharp
+{
+    public partial class MainWindow : System.Windows.Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        CascadeClassifier faceCascade = new CascadeClassifier("haarcascade_frontalface_alt2.xml");
+
+        private void btnLoad_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Image files | *.jpg;*jpeg;*.png;*.bmp";
+
+            if (openFileDialog.ShowDialog() == true)
+            {
+                using Mat image = new Mat(openFileDialog.FileName, ImreadModes.Color);
+                OpenCvSharp.Rect[] faces = faceCascade.DetectMultiScale(image);
+
+                foreach (OpenCvSharp.Rect face in faces)
+                {
+                    image.Rectangle(face, new Scalar(0, 255, 0), 3);
+                }
+
+                imgMain.Source = image.ToBitmapSource();
+            }
+
+        }
+    }
+}
